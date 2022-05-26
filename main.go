f293eda132c496bb9831e1ce8bd6e97a92849509
nathants <me@nathants.com>
2022-05-26 13:24:28 -1000

    copy emptyMultilineString func from go-hasdefer

--- main.go
+++ main.go
@@ -5,10 +5,44 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 )
 
 // TODO is it worth using ast instead of strings, even with go fmt regularity?
+
+func emptyMultilineStrings(data []byte) []byte {
+outer:
+	for {
+		outs := regexp.MustCompile("(?s)`(.*?)`").FindAllStringIndex(string(data), -1)
+		for _, out := range outs {
+			head := data[:out[0]]
+			mid := data[out[0]:out[1]]
+			empty := true
+			for _, b := range mid {
+				if b != '\n' && b != '`' {
+					empty = false
+				}
+			}
+			if empty {
+				continue
+			}
+			tail := data[out[1]:]
+			count := bytes.Count(mid, []byte("\n"))
+			mid = []byte("`")
+			for i := 0; i < count; i++ {
+				mid = append(mid, []byte("\n")...)
+			}
+			mid = append(mid, []byte("`")...)
+			data = []byte{}
+			data = append(data, head...)
+			data = append(data, mid...)
+			data = append(data, tail...)
+			continue outer
+		}
+		return data
+	}
+}
 
 func main() {
 	if len(os.Args) == 1 || (len(os.Args) > 1 && (os.Args[0] == "-h" || os.Args[0] == "--help" || os.Args[0] == "help")) {
@@ -28,7 +62,9 @@ func main() {
 				os.Exit(1)
 			}
 			switchesWithoutDefault := make(map[int]int) // indentLevel:lineNum
-			lines := strings.Split(stdout.String(), "\n")
+			text := stdout.String()
+			text = string(emptyMultilineStrings([]byte(text)))
+			lines := strings.Split(text, "\n")
 			for lineNum, line := range lines {
 				token := strings.Split(strings.Trim(line, "\t"), " ")[0]
 				indentLevel := 0
